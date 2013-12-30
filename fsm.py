0d635494ef2bcf652f1ca6faf7b111f8561943b7
Andreas Eichner <aeichner@debian>
2013-12-30 06:26:30 +0100

    Added the generator script

---
+++ fsm.py
@@ -0,0 +1,249 @@
+from collections import deque, Iterable, defaultdict
+import copy
+
+"""
+d = defaultdict(list)
+d["word1"].append(1)
+d["word2"].append(2)
+d["word2"].append(3)
+d
+defaultdict(<type 'list'>, {'word1': [1], 'word2': [2, 3]})
+"""
+
+class State:
+	def __init__(self):
+		self.transitions = dict()
+		self.actions = dict()
+		self.id = None
+
+	def setTransition(self, label, target, actions = set()):
+		if not self.transitions.has_key(label):
+			self.transitions[label] = set()
+			self.actions[label] = list()
+		self.transitions[label].add(target)
+		actions = actions if isinstance(actions, Iterable) else [actions]
+		for action in actions:
+			if action not in self.actions[label]: self.actions[label].append(action)
+
+class Fsm:
+	def __init__(self):
+		self.entry = None
+		self.labels = list()
+		self.accepts = set()
+
+	def concat(self, fsm):
+		for state in self.accepts: state.setTransition(None, fsm.entry)
+		self.accepts = fsm.accepts
+		return self
+
+	def union(self, fsm):
+		entry = State()
+		entry.setTransition(None, fsm.entry)
+		entry.setTransition(None, self.entry)
+		self.accepts = self.accepts.union(fsm.accepts)
+		self.entry = entry
+		return self
+
+	@staticmethod
+	def empty():
+		fsm = Fsm()
+		fsm.entry = State()
+		fsm.accepts.add(fsm.entry)
+		return fsm
+
+	def isFinal(self, state):
+		return state in self.accepts
+
+	def makeFinal(self, state):
+		self.accepts.add(state)
+
+	@staticmethod
+	def closure(states):
+		if not isinstance(states, Iterable): states = [states]
+		c = set(states)
+		queue = deque(states)
+		while len(queue) > 0:
+			state = queue.popleft()
+			if state.transitions.has_key(None):
+				targets = state.transitions[None]
+				for target in targets:
+					if target not in c:
+						queue.append(target)
+						c.add(target)
+#		print "closure has %d states" % len(c)
+		return c
+
+	def dump(self):
+		visited = set([self.entry])
+		queue = deque([self.entry])
+		self.entry.id = 0
+		id = 1
+		while len(queue) > 0:
+			state = queue.popleft()
+			line = ""
+			line += "%s\t%s%d%s| " % ("entry:" if state == self.entry else "", "[" if self.isFinal(state) else " ", state.id, "]" if self.isFinal(state) else " ")
+			first = True
+			transitions = state.transitions
+			for label, targets in transitions.iteritems():
+				line += "%s%s -> " % ("" if first else ", ", label)
+				first = False
+				idx = False
+				for target in targets:
+					if target not in visited:
+						target.id = id
+						id += 1
+						queue.append(target)
+						visited.add(target)
+					line += "%s%d" % (", " if idx else "", target.id)
+					idx = True
+				state.actions[label]
+				idx = False
+				for action in state.actions[label]:
+					line += "%s%s" % (", " if idx else " / ", action)
+					idx = True
+			print line
+
+	def determinize(self):
+		dfa = Fsm()
+		dfa.entry = State()
+		sets = {0: {'dfaState': dfa.entry, 'set': self.closure(self.entry)}}
+		for state in sets[0]['set']:
+			if self.isFinal(state):
+				dfa.makeFinal(dfa.entry)
+				break
+		queue = deque([0])
+		while len(queue) > 0:
+			i = queue.popleft()
+#			print "Processing DFA state/NFA state set %d" % i
+			transitions = dict()
+			actions = dict()
+			for nfaState in sets[i]['set']:
+#				print "  Processing state %d" % nfaState.id
+				for label, targets in nfaState.transitions.iteritems():
+					if label is None: continue
+					if not transitions.has_key(label):
+						transitions[label] = set()
+						actions[label] = set()
+					transitions[label] = transitions[label].union(targets)
+					if nfaState.actions.has_key(label):
+						actions[label] = actions[label].union(nfaState.actions[label])
+
+			for nfaState in sets[i]['set']:
+				if not nfaState.transitions.has_key(None): continue
+				for label, targets in transitions.iteritems():
+					actions[label] = actions[label].union(nfaState.actions[None])
+
+			for label, targets in transitions.iteritems():
+				targets = self.closure(targets)
+				for target in targets:
+					if not target.transitions.has_key(None): continue
+#					print "  adding actions for the empty word from state %d" % target.id
+					actions[label] = actions[label].union(target.actions[None])
+
+				is_final = False
+				for s in targets:
+					if self.isFinal(s):
+						is_final = True
+						break
+				new_state = None
+				for t in sets.itervalues():
+					if t['set'] == targets:
+						new_state = t['dfaState']
+						break
+				if new_state is None:
+					new_state = State()
+					new_state.id = len(sets)
+					sets[new_state.id] = {'dfaState': new_state, 'set': targets}
+					if is_final: dfa.makeFinal(new_state)
+					queue.append(new_state.id)
+				line = ""
+				for action in actions[label]: line += ", %d" % action
+#				print "  adding transition for label %s to state %d with actions (%s)" % (label, new_state.id, line)
+				sets[i]['dfaState'].setTransition(label, new_state, actions[label])
+		return dfa
+
+class XMLFsm(Fsm):
+	def __init__(self):
+		Fsm.__init__(self)
+		pass
+
+	@staticmethod
+	def sequence(fsms, ea = list(), la = list()):
+		fsm = XMLFsm()
+		fsm.entry = State()
+		fsm.makeFinal(fsm.entry)
+		fsms.reverse()
+		for idx, machine in enumerate(fsms):
+			for accept in machine.accepts:
+				accept.setTransition(None, fsm.entry, la if idx == 0 else set())
+			fsm.entry = machine.entry
+		entry = State()
+		entry.setTransition(None, fsm.entry, ea)
+		fsm.entry = entry
+		return fsm
+
+	@staticmethod
+	def choice(fsms, ea = list(), la = list()):
+		fsm = XMLFsm()
+		fsm.entry = State()
+		leave = State()
+		for machine in fsms:
+			fsm.entry.setTransition(None, machine.entry, ea)
+			for accept in machine.accepts: accept.setTransition(None, leave, la)
+		fsm.makeFinal(leave)
+		return fsm
+
+	@staticmethod
+	def element(name, content, ea = list(), la = list()):
+		fsm = XMLFsm()
+		fsm.entry = State()
+		fsm.entry.setTransition(name, content.entry, ea)
+		leave = State()
+		for state in content.accepts: state.setTransition("/" + name, leave, la)
+		fsm.makeFinal(leave)
+		return fsm
+
+	@staticmethod
+	def particle(term, minOccurs, maxOccurs):
+		if maxOccurs == "unbounded":
+#			print "Building unbounded part"
+			a = copy.deepcopy(term)
+			for accept in a.accepts:
+				accept.setTransition(None, a.entry)
+			a.accepts = set([a.entry])
+		else:
+#			print "Building optional part %d times" % (maxOccurs - minOccurs)
+			a = XMLFsm.empty()
+			leave = a.entry
+			for i in range(0, maxOccurs - minOccurs):
+				c = copy.deepcopy(term)
+				c.concat(a)
+				c.entry.setTransition(None, leave)
+				a = c
+		if minOccurs > 0:
+#			print "Building the mandatory part %d times" % minOccurs
+			b = XMLFsm.empty()
+			for i in range(0, minOccurs):
+#				print "cloning part %d" % i
+				c = copy.deepcopy(term)
+#				c.dump()
+#				print "concating to existing part"
+				b.concat(c)
+#				b.dump()
+			if maxOccurs == "unbounded" or maxOccurs - minOccurs > 0:
+				b.concat(a)
+			return b
+		return a
+"""
+fsm = XMLFsm.choice([
+	XMLFsm.element("Foo", XMLFsm.empty(), [1, 2], [7]),
+	XMLFsm.element("Bar", XMLFsm.empty(), [2, 3], [6]),
+	XMLFsm.element("Baz", XMLFsm.empty(), [3, 4], [5])
+	], [8], [9])
+#fsm = XMLFsm.element("Baz", XMLFsm.empty(), [1], [2])
+fsm.dump()
+fsm = XMLFsm.particle(fsm, 0, "unbounded")
+fsm.dump()
+dfa = fsm.determinize()
+dfa.dump()
+"""
---
+++ xsdcc.py
@@ -0,0 +1,226 @@
+#!/usr/bin/python
+
+import libxml2, sys, os
+import urlparse
+sys.path.append(".")
+sys.setrecursionlimit(10000)
+from fsm import XMLFsm, State
+
+class switch(object):
+	def __init__(self, value):
+		self.value = value
+		self.fall = False
+
+	def __iter__(self):
+		yield self.match
+		raise StopIteration
+
+	def match(self, *args):
+		if self.fall or not args:
+			return True
+		elif self.value in args:
+			self.fall = True
+			return True
+		else:
+			return False
+
+class XSCompiler:
+	XSC_NS = "urn:application:xsc"
+	def __init__(self):
+		self.subgraphs = dict()
+		self.substs = dict()
+		self.Decls = {0: dict(), 1: dict(), 2: dict()}
+		self.declTypes = dict(attribute=0, element=1, complexType=2, attributeGroup=0, group=1, simpleType=2)
+		self.loadedSchemas = set()
+		self.definitions = dict()
+		self.Decls[2] = {
+			"{http://www.w3.org/2001/XMLSchema}string": None,
+			"{http://www.w3.org/2001/XMLSchema}double": None,
+			"{http://www.w3.org/2001/XMLSchema}boolean": None,
+			"{http://www.w3.org/2001/XMLSchema}integer": None,
+			"{http://www.w3.org/2001/XMLSchema}positiveInteger": None,
+			"{http://www.w3.org/2001/XMLSchema}nonNegativeInteger": None,
+			"{http://www.w3.org/2001/XMLSchema}decimal": None,
+			"{http://www.w3.org/2001/XMLSchema}dateTime": None,
+			"{http://www.w3.org/2001/XMLSchema}unsignedLong": None,
+			"{http://www.w3.org/2001/XMLSchema}token": None,
+			"{http://www.w3.org/2001/XMLSchema}normalizedString": None
+		}
+
+
+	def expandQName(self, node, qname):
+		prefix, localname = qname.split(":")
+		if localname is None:
+			prefix, localname = localname, prefix
+		return "{%s}%s" % (node.searchNs(node.get_doc(), prefix).content, localname)
+
+	def importDef(self, node, targetNamespace):
+		qname = "{%s}%s" % (targetNamespace, node.prop("name"))
+		self.Decls[self.declTypes[node.name]][qname] = node
+#		print "Registered type %s" % qname
+		subst = node.prop("substitutionGroup")
+		if bool(subst):
+			subst = self.expandQName(node, subst)
+			if not self.substs.has_key(subst): self.substs[subst] = set()
+			self.substs[subst].add(node)
+#			print "  added %s as substitut for %s" % (qname, subst)
+
+	def loadSchema(self, uri, targetNamespace = None):
+		if uri in self.loadedSchemas: return
+		print "Loading schema file: %s" % uri
+		self.loadedSchemas.add(uri)
+
+		doc = libxml2.readFile(uri, None, options = libxml2.XML_PARSE_NOBLANKS)
+		root = doc.getRootElement()
+		xpath = doc.xpathNewContext()
+
+		result = xpath.xpathEval("/*[local-name()='schema']/*[local-name()='include' or local-name()='import']")
+		for node in result:
+			url = urlparse.urlparse(node.prop("schemaLocation"))
+			if bool(url.scheme): continue
+			loc = os.path.normpath(os.path.join(os.path.dirname(uri), url.path))
+			self.loadSchema(loc, targetNamespace if node.name == "include" else None)
+
+		if targetNamespace is None:
+			targetNamespace = root.prop("targetNamespace")
+
+		result = xpath.xpathEval("/*[local-name()='schema']/*")
+		for node in result:
+			if node.name not in self.declTypes: continue
+			self.importDef(node, targetNamespace)
+
+	def targetNamespace(self, node):
+		return node.get_doc().getRootElement().prop("targetNamespace")
+
+	def createContentModel(self, node, _stack = list()):
+		name = node.prop("name")
+		minOccurs = node.prop("minOccurs")
+		minOccurs = 1 if minOccurs is None else int(minOccurs)
+		maxOccurs = node.prop("maxOccurs")
+		maxOccurs = 1 if maxOccurs is None else (maxOccurs if maxOccurs == "unbounded" else int(maxOccurs))
+		fsm = None
+		print "%s: '%s' (%s, %s) [%d]" % (node.name, name, minOccurs, maxOccurs, len(_stack))
+		if _stack.count(node) > 0:
+			print "*** recursion detected ***"
+			return XMLFsm.empty()
+
+		stack = list(_stack)
+		stack.append(node)
+		for case in switch(node.name):
+			if case("element"):
+				if node.prop("ref") is not None:
+					ref = self.Decls[1][self.expandQName(node, node.prop("ref"))]
+					if ref is None: raise BaseException("Referenced element not known: %s" % node.prop("ref"))
+					return XMLFsm.particle(self.createContentModel(ref, stack), minOccurs, maxOccurs)
+
+				name = node.prop("name")
+				if name is None: raise BaseException("Element requires a name")
+				qname = "{%s}%s" % (self.targetNamespace(node), name)
+				if self.subgraphs.has_key(qname) > 0:
+					print "*** call to subgraph %s ***" % qname
+					fsm = XMLFsm()
+					fsm.entry = State()
+					leave = State()
+					fsm.entry.setTransition(qname, leave)
+					fsm.makeFinal(leave)
+					return XMLFsm.particle(fsm, minOccurs, maxOccurs)
+
+				if node.prop("abstract") == "true":
+					if self.substs.has_key(qname):
+						content = []
+						for child in self.substs[qname]:
+							content.append(self.createContentModel(child, stack))
+						if len(content) > 0:
+							return XMLFsm.particle(XMLFsm.choice(content), minOccurs, maxOccurs)
+					return XMLFsm.empty()
+
+				content = None
+				if node.prop("type") is not None:
+					typename = self.expandQName(node, node.prop("type"))
+					if not self.Decls[2].has_key(typename):
+						raise BaseException("Unknown type %s" % typename)
+					if self.Decls[2][typename] is None:
+						# a simple predefined type
+						return XMLFsm.empty()
+					content = self.createContentModel(self.Decls[2][typename], stack)
+				else:
+					for child in node.children:
+						if child.name in ("simpleType", "complexType"):
+							content = self.createContentModel(child, stack)
+							break
+				if content is None: content = XMLFsm.empty()
+				return XMLFsm.particle(XMLFsm.element(name, content), minOccurs, maxOccurs);
+				break
+
+			if case("complexType"):
+				content = None
+				if node.children is not None:
+					for child in node.children:
+						if child.name in ("simpleContent", "complexContent", "group", "choice", "sequence", "all"):
+							content = self.createContentModel(child, stack)
+							break
+				return XMLFsm.empty() if content is None else XMLFsm.particle(content, minOccurs, maxOccurs)
+				break
+
+			if case("sequence", "choice"):
+				content = []
+				if node.children is not None:
+					for child in node.children:
+						if child.name in ("element", "group", "choice", "sequence", "any"):
+							content.append(self.createContentModel(child, stack))
+				if len(content) == 0:
+					content = XMLFsm.empty()
+				else:
+					content = XMLFsm.sequence(content) if node.name == "sequence" else XMLFsm.choice(content)
+				return XMLFsm.particle(content, minOccurs, maxOccurs)
+				break
+
+			if case("complexContent"):
+				content = None
+				if node.children is not None:
+					for child in node.children:
+						if child.name in ("extension", "restriction"):
+							content = self.createContentModel(child, stack)
+							break
+				return XMLFsm.empty() if content is None else content
+				break
+
+			if case("extension", "restriction"):
+				if node.name == "extension":
+					qname = self.expandQName(node, node.prop("base"))
+					base = self.Decls[2][qname]
+					baseContent = XMLFsm.empty() if base is None else self.createContentModel(base, stack)
+				else:
+					baseContent = XMLFsm.empty()
+				content = None
+				if node.children is not None:
+					for child in node.children:
+						if child.name in ("group", "choice", "sequence"):
+							content = self.createContentModel(child, stack)
+							break
+				if content is None:
+					fsm = baseContent
+				else:
+					fsm = XMLFsm.sequence([baseContent, content])
+				return fsm
+				break
+
+			if case("any"):
+				return XMLFsm.particle(XMLFsm.element("*", XMLFsm.empty()), minOccurs, maxOccurs)
+				break
+
+			if case():
+				raise BaseException("Unknown schema object: %s" % node.name)
+		return fsm
+
+cc = XSCompiler()
+cc.loadSchema(os.path.normpath(sys.argv[1]))
+cc.subgraphs.update({
+	"{http://www.opengis.net/ogc}expression": None,
+	"{http://www.opengis.net/se}Graphic": None,
+	"{http://www.opengis.net/gml}_Geometry": None
+})
+nfa = cc.createContentModel(cc.Decls[1]["{http://www.opengis.net/se}LineSymbolizer"])
+#nfa.dump()
+dfa = nfa.determinize()
+dfa.dump()
