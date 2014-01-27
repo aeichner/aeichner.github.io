f0dacde214fc146f49c047591ff4a85f8f23326e
Andreas Eichner <aeichner@freenet.de>
2014-01-27 15:48:27 +0100

    Make labels and actions symbolic

--- xsdcc.py
+++ xsdcc.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
-import libxml2, sys, os
+from collections import deque
+import libxml2, sys, os, re
 import urlparse
 sys.path.append(".")
 sys.setrecursionlimit(10000)
@@ -46,7 +47,9 @@ class XSCompiler:
 			"{http://www.w3.org/2001/XMLSchema}token": None,
 			"{http://www.w3.org/2001/XMLSchema}normalizedString": None
 		}
-
+		self.namespaces = []
+		self.elements = [("/")]
+		self.actions = []
 
 	def expandQName(self, node, qname):
 		prefix, localname = qname.split(":")
@@ -91,11 +94,150 @@ class XSCompiler:
 
 	def targetNamespace(self, node):
 		return node.get_doc().getRootElement().prop("targetNamespace")
+
+	def getElementId(self, namespace, localname):
+		try:
+			namespaceId = self.namespaces.index(namespace)
+		except ValueError:
+			namespaceId = len(self.namespaces)
+			self.namespaces.append(namespace)
+
+		element = (namespaceId, localname)
+		try:
+			elementId = self.elements.index(element)
+		except ValueError:
+			elementId = len(self.elements)
+			self.elements.append(element)
+		return elementId
 	
-	@staticmethod
-	def getActions(s):
+	def getActions(self, s):
 		if s is None: return []
-		return [x for x in s.split(" ")]
+		l = []
+		for i in re.finditer(r"\s*(\w+)\s*\(\s*((\w+\s*(,\s*\w+)*)?\s*)\)", s):
+			args = "_".join([a.group(1).replace("_", "__") for a in re.finditer(r",?\s*(\w+)", i.group(2))])
+			action = "%s%s%s" % (i.group(1).replace("_", "__"), "_" if len(args) > 0 else "", args)
+			try:
+				actionId = self.actions.index(action)
+			except ValueError:
+				actionId = len(self.actions)
+				self.actions.append(action)
+			l.append(actionId)
+#		print "Action IN: %s | OUT: %s" % (s, l)
+		return l
+
+	def element(self, namespace, localname, content):
+		elementId = self.getElementId(namespace, localname)
+		fsm = XMLFsm()
+		fsm.entry = State()
+		fsm.entry.addTransition(elementId, content.entry)
+		leave = State()
+		for state in content.accepts:
+			state.addTransition(0, leave)
+		fsm.accepts = set([leave])
+		return fsm
+
+	def dump2(self):
+		visited = set([self.entry])
+		inqueue = deque([self.entry])
+		outqueue = deque()
+		labels = dict()
+		substs = set()
+		id = 1
+		labelId = 0
+		while len(inqueue) > 0:
+			state = inqueue.popleft()
+			if state not in self.accepts:
+				state.id = id
+				id += 1
+				outqueue.append(state)
+
+			transitions = dict((trans.label, state.statesForLabel(trans.label)) for trans in state.transitions)
+			for label, targets in transitions.iteritems():
+				if not (label.startswith("/") or label.startswith('!')) and label not in labels:
+					labels[label] = labelId
+					labelId += 1
+				elif label.startswith('!'):
+					substs.add(label)
+				for target in targets:
+					if target not in visited:
+						inqueue.append(target)
+						visited.add(target)
+		for state in self.accepts:
+			state.id = id
+			id += 1
+			outqueue.append(state)
+		for label in substs:
+			labels[label] = labelId
+			labelId += 1
+
+		first_final = min(map(lambda x: x.id, self.accepts))
+		token_list = []
+		token_offset = [0]
+		token_length = [0]
+		target_list = [0, 0]
+		target_offset = [0]
+
+		print "first_final = %d" % first_final
+		while len(outqueue) > 0:
+			state = outqueue.popleft()
+			onClose = 0
+			nTokens = 0
+			t_list = []
+			token_offset.append(len(token_list))
+			transitions = dict((trans.label, state.statesForLabel(trans.label)) for trans in state.transitions)
+			for label, targets in transitions.iteritems():
+				target = reduce(lambda x, y: x if y is None else y, targets)
+				if label.startswith('/'):
+					onClose = target.id
+				else:
+					token_list.append(labels[label])
+					nTokens += 1
+					t_list.append(target.id)
+			target_offset.append(len(target_list))
+			target_list.append(onClose)
+			target_list.extend(t_list)
+			target_list.append(0)		# default transition
+			token_length.append(len(t_list))
+
+		print "token_list := %s\n" % str(token_list)
+		print "token_offset := %s\n" % str(token_offset)
+		print "token_length := %s\n" % str(token_length)
+		print "target_list := %s\n" % str(target_list)
+		print "target_offset := %s\n" % str(target_offset)
+		for index, token in dict(zip(labels.values(), labels.keys())).iteritems():
+			print "{0, %s}" % token
+
+	def dump(self, nfa):
+		visited = set([nfa.entry])
+		queue = deque([nfa.entry])
+		nfa.entry.id = 1
+		id = 2
+		while len(queue) > 0:
+			state = queue.popleft()
+			line = ""
+			line += "%s\t%s%d%s| " % ("entry:" if state == nfa.entry else "", "[" if state in nfa.accepts else " ", state.id, "]" if state in nfa.accepts else " ")
+			first = True
+			transitions = dict((trans.label, state.statesForLabel(trans.label)) for trans in state.transitions)
+			for label, targets in transitions.iteritems():
+				line += "%s%s -> " % ("" if first else ", ", self.elements[label][1] if label else "/")
+				first = False
+				idx = False
+				for target in targets:
+					if target not in visited:
+						target.id = id
+						id += 1
+						queue.append(target)
+						visited.add(target)
+					line += "%s%d" % (", " if idx else "", target.id)
+					idx = True
+				actions = []
+				for trans in state.transForLabel(label):
+					actions.extend([t for t in trans.actions if t not in actions])
+				idx = False
+				for action in actions:
+					line += "%s%s" % (", " if idx else " / ", self.actions[action])
+					idx = True
+			print line
 
 	def createContentModel(self, node, ea, la, _stack = list()):
 		name = node.prop("name")
@@ -106,7 +248,7 @@ class XSCompiler:
 		fsm = None
 		ea.extend(self.getActions(node.prop("enter")))
 		la[:0] = self.getActions(node.prop("leave"))
-		print "%s%s: '%s' (%s, %s) %s | %s" % (len(_stack)* "  ", node.name, name, minOccurs, maxOccurs, ea, la)
+		print "%s%s: '%s' (%s, %s) %s | %s" % (len(_stack)* "  ", node.name, name, minOccurs, maxOccurs, [self.actions[e] for e in ea], [self.actions[a] for a in la])
 		if _stack.count(node) > 0:
 			print "*** recursion detected ***"
 			return XMLFsm().empty()
@@ -128,7 +270,7 @@ class XSCompiler:
 					fsm = XMLFsm()
 					fsm.entry = State()
 					leave = State()
-					fsm.entry.addTransition("%s%s" % ('!' if node.prop("abstract") == "true" else "", qname), leave)
+					fsm.entry.addTransition(self.getElementId(self.targetNamespace(node), "%s%s" % ('!' if node.prop("abstract") == "true" else "", name)), leave)
 					fsm.accepts.add(leave)
 					return fsm.particle(minOccurs, maxOccurs)
 
@@ -156,7 +298,7 @@ class XSCompiler:
 							break
 						child = child.next
 				if content is None: content = XMLFsm().empty()
-				return XMLFsm().element(name, content).apply(ea, la).particle(minOccurs, maxOccurs)
+				return self.element(self.targetNamespace(node), name, content).apply(ea, la).particle(minOccurs, maxOccurs)
 				break
 
 			if case("simpleType"):
@@ -218,31 +360,29 @@ class XSCompiler:
 					fsm = baseContent
 				else:
 					fsm = baseContent.concat(content)
-#					if node.prop("base") == "se:SymbolizerType":
-#						print "**********"
-##						baseContent.dump()
-#						content.dump()
-#						fsm.dump()
 				return fsm
 				break
 
 			if case("any"):
-				return XMLFsm().element("*", XMLFsm().empty()).particle(minOccurs, maxOccurs)
+				return self.element(self.targetNamespace(node), "*", XMLFsm().empty()).particle(minOccurs, maxOccurs)
 				break
 
 			if case():
 				raise BaseException("Unknown schema object: %s" % node.name)
 		return fsm
 
-cc = XSCompiler()
-cc.loadSchema(os.path.normpath(sys.argv[1]))
-cc.subgraphs.update({
-	"{http://www.opengis.net/ogc}expression": None,
-	"{http://www.opengis.net/se}Graphic": None,
-	"{http://www.opengis.net/gml}_Geometry": None
-})
-nfa = cc.createContentModel(cc.Decls[1]["{http://www.opengis.net/se}LineSymbolizer"], [], [])
-#nfa.dump()
-dfa = nfa.determinize().minimize()
-dfa.dump()
-dfa.dump2()
+if __name__ == "__main__":
+	cc = XSCompiler()
+	cc.loadSchema(os.path.normpath(sys.argv[1]))
+	cc.subgraphs.update({
+		"{http://www.opengis.net/ogc}expression": None,
+		"{http://www.opengis.net/se}Graphic": None,
+	#	"{http://www.opengis.net/se}LineSymbolizer": None,
+	#	"{http://www.opengis.net/se}PolygonSymbolizer": None,
+		"{http://www.opengis.net/gml}_Geometry": None
+	})
+	nfa = cc.createContentModel(cc.Decls[1]["{http://www.opengis.net/se}LineSymbolizer"], [], [])
+	#nfa.dump()
+	dfa = nfa.determinize().minimize()
+	cc.dump(dfa)
+	#dfa.dump2()
